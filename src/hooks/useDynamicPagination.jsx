235c16da519d6dc65f8799c997733361f4dce7de
DimaEff <fominienkovd@users.noreply.github.com>
2021-07-16 18:20:24 +0300

    refactor useDynamicPagination

--- src/hooks/useDynamicPagination.jsx
+++ src/hooks/useDynamicPagination.jsx
@@ -15,7 +15,7 @@ const useDynamicPagination = (requestFunction, totalCount, startPage = 1, limit 
         fetchData();
     }, [currentPage, limit, setIsFetching, requestFunction])
 
-    const scrollHandler = (e) => {
+    const scrollHandler = (e) => {;
         const scrollPosition = e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight);
         if (scrollPosition < 100 && currentPage < pagesCount) {
             setCurrentPage(p => p + 1);
